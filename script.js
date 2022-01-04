b07cb888956a3b4d8904d96b9b6571538d977352
Tevyn <94698766+tevynaugustin@users.noreply.github.com>
2022-01-04 20:41:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?landscape')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
